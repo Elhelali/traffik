467f4bf79b7d4a67e1276fdb390cb8ea2c71c6d0
Ali <ali.elhelali@equiinet.com>
2021-12-07 16:06:35 -0800

    first commit

--- config.py
+++
@@ -1,4 +0,0 @@
-mongo_username="adminman"
-mongo_password="adminmanhasaplan"
-mongo_url="jaqagencyinstance.klf7l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-SECRET_KEY = "FAAIFOASKHDADgJLO"
